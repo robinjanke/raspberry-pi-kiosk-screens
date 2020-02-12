17bf2e1597edda0bc86804567058dd15316b4c5e
Robin Janke <mail@robin-janke.de>
2020-02-12 16:48:53 +0100

    fixed some naming issues

--- setup_client.py
+++ setup_client.py
@@ -12,7 +12,7 @@ def ask_and_write_to_file(text, filename):
 
 jira_username_filename = "data/jira_user.txt"
 jira_password_filename = "data/jira_password.txt"
-jira_namespace_filename = "data/jira_url.txt"
+jira_url_filename = "data/jira_url.txt"
 jira_dashboard_filename = "data/jira_dashboard.txt"
 
 webdriver_path = ""
@@ -32,15 +32,14 @@ for package in ["chromium-driver", "ufw", "vim"]:
 
 ask_and_write_to_file("Jira username", jira_username_filename)
 ask_and_write_to_file("Jira password", jira_password_filename)
-ask_and_write_to_file("Jira namespace", jira_namespace_filename)
-ask_and_write_to_file("Jira dashboard", jira_dashboard_filename)
+ask_and_write_to_file("Jira url (https://xxx.atlassian.net)", jira_url_filename)
+ask_and_write_to_file("Jira dashboard ID", jira_dashboard_filename)
 
 print("Setup autostart to launch {} file".format(script_filename))
 dashboard_file = open(launch_dashboard_filename, "r")
 current_dir = os.path.dirname(os.path.realpath(__file__))
 dashboard_file_content = dashboard_file.read()
-dashboard_file_content = dashboard_file_content.replace("CURRENT_DIR", current_dir)
-dashboard_file_content = dashboard_file_content.replace("FILENAME", script_filename)
+dashboard_file_content = dashboard_file_content.format(CURRENT_DIR=current_dir, FILENAME=script_filename)
 subprocess.check_output(["sudo", "touch", launch_dashboard_filename_etc])
 subprocess.check_output(["sudo", "chmod", "0777", launch_dashboard_filename_etc])
 subprocess.check_output(["sudo", "chown", "root:root", launch_dashboard_filename_etc])
