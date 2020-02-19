bdf91915e887b256554bc2deaf0552eb8f1df5ca
Robin Janke <mail@robin-janke.de>
2020-02-19 15:41:22 +0100

    added jenkins support

--- launch_dashboard.py
+++ launch_dashboard.py
@@ -3,35 +3,68 @@ import time
 
 from selenium.webdriver.common.keys import Keys
 
-email = open("data/jira_user.txt", "r").read()
-password = open("data/jira_password.txt", "r").read()
-url = open("data/jira_url.txt", "r").read()
-dashboard = open("data/jira_dashboard.txt", "r").read()
 
-options = webdriver.ChromeOptions()
-options.add_argument(argument="-kiosk")
-options.add_argument(argument="--no-sandbox")
-options.add_argument(argument="--disable-dev-shm-usage")
-options.add_argument(argument="--disable-gpu")
-options.add_argument(argument="--window-size=1920,1080")
-options.add_argument(argument="disable-infobars")
-options.add_experimental_option("useAutomationExtension", False)
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option("prefs", {
-    "credentials_enable_service": False,
-    "profile.password_manager_enabled": False
-})
+def launch_jira(driver):
+    email = open("data/jira_user.txt", "r").read()
+    password = open("data/jira_password.txt", "r").read()
+    url = open("data/jira_url.txt", "r").read()
+    dashboard = open("data/jira_dashboard.txt", "r").read()
 
-chromedriver = webdriver.Chrome(options=options)
-chromedriver.get(url)
+    driver.get(url)
 
-email_input = chromedriver.find_element_by_id("username")
-email_input.send_keys(email)
-email_input.send_keys(Keys.ENTER)
-time.sleep(3)
-password_input = chromedriver.find_element_by_id("password")
-password_input.send_keys(password)
-password_input.send_keys(Keys.ENTER)
-time.sleep(10)
+    email_input = driver.find_element_by_id("username")
+    email_input.send_keys(email)
+    email_input.send_keys(Keys.ENTER)
+    time.sleep(3)
+    password_input = driver.find_element_by_id("password")
+    password_input.send_keys(password)
+    password_input.send_keys(Keys.ENTER)
+    time.sleep(10)
 
-chromedriver.get("{}/plugins/servlet/Wallboard/?dashboardId={}".format(url, dashboard))
+    driver.get("{}/plugins/servlet/Wallboard/?dashboardId={}".format(url, dashboard))
+
+
+def launch_jenkins(driver):
+    username = open("data/jenkins_username.txt", "r").read()
+    password = open("data/jenkins_password.txt", "r").read()
+    url = open("data/jenkins_url.txt", "r").read()
+    view = open("data/jenkins_view.txt", "r").read()
+
+    driver.get(url)
+
+    username_input = driver.find_element_by_name("j_username")
+    username_input.send_keys(username)
+    username_input.send_keys(Keys.ENTER)
+    time.sleep(3)
+    password_input = driver.find_element_by_name("j_password")
+    password_input.send_keys(password)
+    password_input.send_keys(Keys.ENTER)
+    time.sleep(10)
+
+    driver.get("{}/view/{}".format(url, view))
+
+
+if __name__ == "__main__":
+    mode = open("data/mode.txt", "r").read()
+
+    options = webdriver.ChromeOptions()
+    options.add_argument(argument="-kiosk")
+    options.add_argument(argument="--no-sandbox")
+    options.add_argument(argument="--disable-dev-shm-usage")
+    options.add_argument(argument="--disable-gpu")
+    options.add_argument(argument="--window-size=1920,1080")
+    options.add_argument(argument="disable-infobars")
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("prefs", {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False
+    })
+    driver = webdriver.Chrome(options=options)
+
+    if mode == "jira":
+        launch_jira(driver)
+    elif mode == "jenkins":
+        launch_jenkins(driver)
+    else:
+        raise Exception("Not supported")
--- setup_client.py
+++ setup_client.py
@@ -2,6 +2,22 @@ import subprocess
 import os
 import time
 
+jira_username_filename = "data/jira_user.txt"
+jira_password_filename = "data/jira_password.txt"
+jira_url_filename = "data/jira_url.txt"
+jira_dashboard_filename = "data/jira_dashboard.txt"
+
+jenkins_username_filename = "data/jenkins_username.txt"
+jenkins_password_filename = "data/jenkins_password.txt"
+jenkins_url_filename = "data/jenkins_user.txt"
+jenkins_view_filename = "data/jenkins_view.txt"
+
+webdriver_path = ""
+script_filename = "launch_dashboard.py"
+launch_dashboard_filename = "launch_dashboard.desktop"
+launch_dashboard_filename_etc = "/etc/xdg/autostart/launch_dashboard.desktop"
+mode_filename = "data/mode.txt"
+
 
 def ask_and_write_to_file(text, filename):
     print("Please enter: {}".format(text))
@@ -10,45 +26,70 @@ def ask_and_write_to_file(text, filename):
     file.write(content)
 
 
-jira_username_filename = "data/jira_user.txt"
-jira_password_filename = "data/jira_password.txt"
-jira_url_filename = "data/jira_url.txt"
-jira_dashboard_filename = "data/jira_dashboard.txt"
+def ask_for_mode():
+    print("Please enter your mode: jenkins or jira")
+    mode = input()
+    if mode == "jenkins":
+        print("Using mode jenkins")
+    elif mode == "jira":
+        print("Using mode jira")
+    else:
+        ask_for_mode()
+    file = open(mode_filename, "w+")
+    file.write(mode)
+    return mode
 
-webdriver_path = ""
-script_filename = "launch_dashboard.py"
-launch_dashboard_filename = "launch_dashboard.desktop"
-launch_dashboard_filename_etc = "/etc/xdg/autostart/launch_dashboard.desktop"
 
-print("Installing pip packages start")
-subprocess.check_output(["/bin/bash", "-c", "pip3", "install" "-r", "requirements.txt"])
+def setup_jira():
+    ask_and_write_to_file("Jira username", jira_username_filename)
+    ask_and_write_to_file("Jira password", jira_password_filename)
+    ask_and_write_to_file("Jira url (https://xxx.atlassian.net)", jira_url_filename)
+    ask_and_write_to_file("Jira dashboard ID", jira_dashboard_filename)
 
-print("Running apt-get update")
-subprocess.check_output(["sudo", "apt-get", "update", "-y"])
 
-print("Installing packages with apt-get")
-for package in ["chromium-driver", "ufw", "vim", "unclutter"]:
-    subprocess.check_output(["sudo", "apt-get", "install", "-y", package])
+def setup_jenkins():
+    ask_and_write_to_file("Jenkins username", jenkins_username_filename)
+    ask_and_write_to_file("Jenkins password", jenkins_password_filename)
+    ask_and_write_to_file("Jenkins url (https://jenkins.example.com)", jenkins_url_filename)
+    ask_and_write_to_file("Jenkins view name (can be found in the url)", jenkins_view_filename)
 
-ask_and_write_to_file("Jira username", jira_username_filename)
-ask_and_write_to_file("Jira password", jira_password_filename)
-ask_and_write_to_file("Jira url (https://xxx.atlassian.net)", jira_url_filename)
-ask_and_write_to_file("Jira dashboard ID", jira_dashboard_filename)
 
-print("Setup autostart to launch {} file".format(script_filename))
-dashboard_file = open(launch_dashboard_filename, "r")
-current_dir = os.path.dirname(os.path.realpath(__file__))
-dashboard_file_content = dashboard_file.read()
-dashboard_file_content = dashboard_file_content.format(CURRENT_DIR=current_dir, FILENAME=script_filename)
-subprocess.check_output(["sudo", "touch", launch_dashboard_filename_etc])
-subprocess.check_output(["sudo", "chmod", "0777", launch_dashboard_filename_etc])
-subprocess.check_output(["sudo", "chown", "root:root", launch_dashboard_filename_etc])
-open(launch_dashboard_filename_etc, "w+").write(dashboard_file_content)
-subprocess.check_output(["sudo", "chmod", "0755", launch_dashboard_filename_etc])
+def setup():
+    print("Installing pip packages start")
+    subprocess.check_output(["/bin/bash", "-c", "pip3", "install" "-r", "requirements.txt"])
 
-for second in range(10, 0):
-    print("Rebooting in {} seconds".format(second))
-    time.sleep(1)
+    print("Running apt-get update")
+    subprocess.check_output(["sudo", "apt-get", "update", "-y"])
 
-print("Rebooting now")
-subprocess.check_output(["sudo", "reboot"])
+    print("Installing packages with apt-get")
+    for package in ["chromium-driver", "ufw", "vim", "unclutter"]:
+        subprocess.check_output(["sudo", "apt-get", "install", "-y", package])
+
+    mode = ask_for_mode()
+
+    if mode == "jira":
+        setup_jira()
+    elif mode == "jenkins":
+        setup_jenkins()
+
+    print("Setup autostart to launch {} file".format(script_filename))
+    dashboard_file = open(launch_dashboard_filename, "r")
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    dashboard_file_content = dashboard_file.read()
+    dashboard_file_content = dashboard_file_content.format(CURRENT_DIR=current_dir, FILENAME=script_filename)
+    subprocess.check_output(["sudo", "touch", launch_dashboard_filename_etc])
+    subprocess.check_output(["sudo", "chmod", "0777", launch_dashboard_filename_etc])
+    subprocess.check_output(["sudo", "chown", "root:root", launch_dashboard_filename_etc])
+    open(launch_dashboard_filename_etc, "w+").write(dashboard_file_content)
+    subprocess.check_output(["sudo", "chmod", "0755", launch_dashboard_filename_etc])
+
+    for second in range(10, 0):
+        print("Rebooting in {} seconds".format(second))
+        time.sleep(1)
+
+    print("Rebooting now")
+    subprocess.check_output(["sudo", "reboot"])
+
+
+if __name__ == "__main__":
+    setup()
